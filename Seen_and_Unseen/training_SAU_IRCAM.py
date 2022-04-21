b9b4eb2d01411b9c28a16eb5e21eebc2b0eed4ec
LucStk <luc.sterkers@laposte.net>
2022-04-21 16:19:27 +0200

    sqdf

--- Seen_and_Unseen/training_SAU_IRCAM.py
+++ Seen_and_Unseen/training_SAU_IRCAM.py
@@ -121,9 +121,9 @@ with tf.device(comp_device) :
     len_test_dataloader  = len(test_dataloader)
 
     #Utilisation data_queue
-    if False:
+    if True:
         print("begin data_queue")
-        data_queue = tf.keras.utils.OrderedEnqueuer(train_dataloader, use_multiprocessing=True, shuffle=True)
+        data_queue = tf.keras.utils.OrderedEnqueuer(train_dataloader, use_multiprocessing=False, shuffle=True)
         data_queue.start()
         train_dataloader = data_queue.get()
 
