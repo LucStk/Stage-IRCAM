c806fbed9d4bcc1fdb19d1498d00aa49c8290c71
LucStk <luc.sterkers@laposte.net>
2022-04-22 11:30:57 +0200

    sdf

--- Seen_and_Unseen/utilitaires/SAU_fit.py
+++ Seen_and_Unseen/utilitaires/SAU_fit.py
@@ -268,6 +268,7 @@ class SAU_GAN(tf.keras.Model):
     self.loss    = loss
 
   def call(self, input):
+    print(input)
     print(len(input.shape))
     (x, phi, y) = input
     x = normalisation(x)
