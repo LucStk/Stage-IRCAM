2d807b08aacd9f22d5b0d979c831f0bbae70a447
LucStk <luc.sterkers@laposte.net>
2022-04-22 17:18:14 +0200

    fit dataloader

--- Seen_and_Unseen/tr_SAU_fit_IRCAM.py
+++ Seen_and_Unseen/tr_SAU_fit_IRCAM.py
@@ -106,7 +106,7 @@ with tf.device(comp_device) :
     len_test_dataloader  = len(test_dataloader)
 
     #Utilisation data_queue
-    if False:
+    if True:
         print("begin data_queue")
         data_queue = tf.keras.utils.OrderedEnqueuer(train_dataloader, use_multiprocessing=False, shuffle=True)
         data_queue.start(4, 20)
