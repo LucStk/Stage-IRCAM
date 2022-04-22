66cca3ef28fb9123b78423aa01282a48b256fec2
LucStk <luc.sterkers@laposte.net>
2022-04-22 11:43:31 +0200

    qsfd

--- Seen_and_Unseen/tr_SAU_fit_IRCAM.py
+++ Seen_and_Unseen/tr_SAU_fit_IRCAM.py
@@ -119,6 +119,6 @@ with tf.device(comp_device) :
     MSE = tf.keras.losses.MeanSquaredError()
 
     SAU.compile(ae_optim,disc_optim,MSE)
-    SAU(train_dataloader[0])
+    print(SAU(train_dataloader[0]))
     #SAU.fit(train_dataloader, epochs = 10) 
 
